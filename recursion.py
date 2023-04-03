bd3abd0e94a93bf545f87b5aff1195029616a745
Imasandi <91633499+Imasandi@users.noreply.github.com>
2023-04-03 15:44:38 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -34,3 +34,33 @@ print("Fibonacci series of 5 numbers is :", end=" ")
 # for loop to print the fibonacci series.
 for i in range(0, n):
     print(fib(i), end=" ")
+
+    
+ """
+Problem 2: Write a program and recurrence relation to find the Factorial of n where n>2 . 
+Mathematical Equation: 
+
+1 if n == 0 or n == 1;      
+f(n) = n*f(n-1) if n> 1;
+Recurrence Relation: 
+
+T(n) = 1 for n = 0
+T(n) = 1 + T(n-1) for n > 0
+Recursive Program: 
+Input: n = 5 
+Output: 
+factorial of 5 is: 120
+ 
+"""
+
+def f(n):
+    if n == 0:
+        return 0
+
+    if n == 0 or n == 1:
+        return 1
+    else :
+        return n * f(n - 1)
+n = 5
+
+print("factorial of 5 is:" ,f(n))
